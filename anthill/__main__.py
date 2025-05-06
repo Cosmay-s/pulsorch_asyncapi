f16da0c03a9b3c083ba2dc17b9804b48e0a4aae7
Sergey Simakov <SaneniRem@yandex.ru>
2025-05-06 17:57:33 +0300

    правки

---
+++ anthill/__main__.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Body
+from uuid import uuid4
+from datetime import datetime, UTC
+import logging
+import uvicorn
+from anthill import schemas, config
+from typing import Annotated
+import asyncio
+
+app = FastAPI()
+logger = logging.getLogger(__name__)
+runs: list[schemas.Run] = []
+
+
+@app.post("/api/v1/srv/runs/")
+async def create_run(
+    job_id: Annotated[int, Body()],
+    code: Annotated[str, Body()],
+    external_status: Annotated[str, Body()],
+    start_time: Annotated[datetime, Body()]
+) -> schemas.Run:
+    now = datetime.now(UTC)
+    run = schemas.Run(
+        run_id=uuid4(),
+        job_id=job_id,
+        external_status=external_status,
+        start_time=start_time,
+        created_at=now,
+        updated_at=now,
+        status=schemas.RunStatus.CREATED
+    )
+    runs.append(run)
+    return run
+
+@app.get("/api/v1/srv/runs/")
+async def get_runs() -> list[schemas.Run]:
+    return runs
+
+
+async def main():
+    server_config = config.create_server_config()
+    uvicorn.run("anthill.__main__:app",
+                host=server_config.host,
+                port=server_config.port,
+                reload=server_config.debug)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ anthill/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from uuid import UUID
+from datetime import datetime
+import enum
+
+
+class RunStatus(enum.Enum):
+    CREATED = "created"
+    SCHEDULED = "scheduled"
+
+
+class Run(BaseModel):
+    run_id: UUID
+    job_id: int
+    external_status: str
+    start_time: datetime
+    created_at: datetime
+    updated_at: datetime
+    status: RunStatus
